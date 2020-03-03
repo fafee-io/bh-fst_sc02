a5783fc842ce8ea0d854285100ec37c2257dfbac
fafee-io <fafee@fafee.io>
2020-03-03 10:34:54 +0100

    03 megoldas

--- lessons/02/elso.js
+++ lessons/02/elso.js
@@ -61,12 +61,13 @@ let prim2 = (n) => {
         ++i;
     }
     return valasz;
-}
+};
 
 console.log('primezés:');
-console.log(prim(4));
+console.log(prim(7));
 console.log(prim(14));
-console.log(prim(9));
+console.log(prim(23));
+console.log(prim(143));
 
 // megszamolja hogy paraméterben kapott 2 szám között
 // hány prim szám van és ezt adja vissza
@@ -78,7 +79,7 @@ let szamol = (a, b) => {
         }
     }
     return counter;
-}
+};
 console.log('szamolas: ' + szamol(2, 10));
 
 /*
@@ -90,3 +91,68 @@ let sum = function(a,b) {
     return a + b;
 };
 */
+console.log("aaa");
+console.log("aaa");
+console.log("aaa");
+
+let tomb = [];
+let elemekkel = [1, 3, 5, 7];
+// DONT DO: let izgalmas = ['a', 1, true, 2, ''];
+
+elemekkel[0]; // = 1
+elemekkel[1]; // = 3
+elemekkel[2]; // = 5
+elemekkel[3]; // = 7
+
+let tombindex = 2;
+elemekkel[tombindex]; // = 5
+
+console.log(tomb);
+console.log(elemekkel);
+console.log(elemekkel[0]);
+console.log(elemekkel[1]);
+console.log(elemekkel[2]);
+console.log(elemekkel[3]);
+console.log(elemekkel[tombindex]);
+console.log(tomb.length);
+console.log(elemekkel.length);
+
+console.log(elemekkel[4]);
+elemekkel[4] = 13; // új elemhez nem ajánlott (mert elrontod az indexet)
+console.log(elemekkel[4]);
+elemekkel.push(17);
+console.log(elemekkel.length); // 6
+console.log(elemekkel[5]);
+elemekkel.push(19, 21, 33);
+console.log(elemekkel.length); // 9
+console.log(elemekkel[8]); // 33
+elemekkel[8] = 42;
+console.log(elemekkel[8]); // 42
+
+console.log('BEJÁRÁS:');
+// elemekkel tömb bejárása
+for (let i = 0; i < elemekkel.length ; ++i) {
+    console.log( elemekkel[i] );
+}
+
+// Maximum Keresés
+// bemenet: tömb, melyben (legalább egy) pozitív számok vannak
+// kimenet a legnagyobb tömbben tárolt érték
+// [2] : 2
+// [1,2,3] : 3
+// [3, 4, 23, 12, 6] : 23
+// [33, 4, 23, 12, 6] : 33
+let maxKer = (t) => {
+    let max = 0; // t[0];
+    for (let i = 0; i < t.length ; ++i) {
+        if (t[i] > max) {
+            max = t[i];
+        }
+    }
+    return max;
+}
+console.log('maxkeresés:')
+console.log( maxKer(elemekkel) );
+console.log( maxKer( [3, 4, 23, 12, 6] ) );
+console.log( maxKer( [33, 4, 23, 12, 6] ) );
+console.log( maxKer( [1, 2, 10, 10, 10, 10, 2, 4, 8] ) );
---
+++ lessons/03/anyag.md
@@ -0,0 +1,26 @@
+### 3. óra
+
+## Synopsis
+
+- Előző óra folytatása, tömbökkel
+- Még egy kis algoritmizálás, ezúttal tömbökkel
+- Git-es letöltés, feladat fájl másolás
+- Izgalmas böngészős feladat megoldás az elkészült algoritmust is felhasználva
+
+## Tömbök
+
+- Létrehozás: `let tomb = [];`
+- Elemek elérése, length
+- Elemek hozzáadása
+- Elemek bejárása
+
+## A maximum keresés algoritmusa
+
+- Ez egy programozási tétel
+- Egyszerűen, for ciklussal
+
+## Feladat a böngészőben
+
+- A meglévő kód által generált div-ek közül, gomb nyomásra színezzük ki:
+- A legnagyobbat pirosra,
+- HF (A legkissebbet zöldre)
