810d670de9bb6231937654a9c9c831b96159167f
Gabor Farkas <resident1987@gmail.com>
2020-03-20 20:39:21 +0100

    2020.03.20 órai anyag

---
+++ lessons/08/js/definitions.js
@@ -0,0 +1,163 @@
+        var hosszuGomb = 1;
+        var normalEsUresGomb = 2;
+
+        var gombsor = {
+            "sorok": [
+                [
+                    {
+                        "id": "mc",
+                        "class": "cbutton mbutton",
+                        "value": "MC"
+                    },
+                    {
+                        "id": "mr",
+                        "class": "cbutton mbutton",
+                        "value": "MR"
+                    },
+                    {
+                        "id": "ms",
+                        "class": "cbutton mbutton",
+                        "value": "MS"
+                    },
+                    {
+                        "id": "mplus",
+                        "class": "cbutton mbutton",
+                        "value": "M+"
+                    },
+                    {
+                        "id": "mminus",
+                        "class": " cbutton mbutton",
+                        "value": "M-"
+                    }
+                ],
+                [
+                    {
+                        "id": "back",
+                        "class": "cbutton opbutton",
+                        "value": "BACK"
+                    },
+                    {
+                        "id": "ce",
+                        "class": "cbutton opbutton",
+                        "value": "CE"
+                    },
+                    {
+                        "id": "c",
+                        "class": "cbutton opbutton",
+                        "value": "C"
+                    },
+                    {
+                        "id": "mplusminus",
+                        "class": "cbutton opbutton",
+                        "value": "+-"
+                    },
+                    {
+                        "id": "gyok",
+                        "class": "cbutton opbutton",
+                        "value": "GYÖK"
+                    }
+                ],
+                [
+                    {
+                        "id": "szam7",
+                        "class": "cbutton szambutton",
+                        "value": "7"
+                    },
+                    {
+                        "id": "szam8",
+                        "class": "cbutton szambutton",
+                        "value": "8"
+                    },
+                    {
+                        "id": "szam9",
+                        "class": "cbutton szambutton",
+                        "value": "9"
+                    },
+                    {
+                        "id": "osztas",
+                        "class": "cbutton opbutton",
+                        "value": "/"
+                    },
+                    {
+                        "id": "szazalek",
+                        "class": "cbutton opbutton",
+                        "value": "%"
+                    }
+                ],
+                [
+                    {
+                        "id": "szam4",
+                        "class": "cbutton szambutton",
+                        "value": "4"
+                    },
+                    {
+                        "id": "szam5",
+                        "class": "cbutton szambutton",
+                        "value": "5"
+                    },
+                    {
+                        "id": "szam6",
+                        "class": "cbutton szambutton",
+                        "value": "6"
+                    },
+                    {
+                        "id": "szorzas",
+                        "class": "cbutton opbutton",
+                        "value": "*"
+                    },
+                    {
+                        "id": "reciprok",
+                        "class": "cbutton opbutton",
+                        "value": "1/x"
+                    }
+                ],
+                [
+                    {
+                        "id": "szam1",
+                        "class": "cbutton szambutton",
+                        "value": "1"
+                    },
+                    {
+                        "id": "szam2",
+                        "class": "cbutton szambutton",
+                        "value": "2"
+                    },
+                    {
+                        "id": "szam3",
+                        "class": "cbutton szambutton",
+                        "value": "3"
+                    },
+                    {
+                        "id": "kivonas",
+                        "class": "cbutton opbutton",
+                        "value": "-",
+                        "tipus": normalEsUresGomb
+                    },
+                    null
+                ],
+                [
+                    {
+                        "id": "szam0",
+                        "class": "cbutton szambutton",
+                        "value": "0",
+                        "tipus": hosszuGomb
+                    },
+                    null,
+                    {
+                        "id": "tizedespont",
+                        "class": "cbutton szambutton",
+                        "value": ","
+                    },
+                    {
+                        "id": "osszeadas",
+                        "class": "cbutton opbutton",
+                        "value": "+"
+                    },
+                    {
+                        "id": "eredmeny",
+                        "class": "cbutton opbutton",
+                        "value": "="
+                    }
+                ],
+            ]
+        };
